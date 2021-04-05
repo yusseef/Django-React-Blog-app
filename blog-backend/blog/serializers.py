78353e214dd296a64e95076afc57b4e1946a4ca6
yusseef <yussef.raouf11@gmail.com>
2021-04-05 19:53:14 +0200

    update settings and libraries

---
+++ blog-backend/blog/serializers.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
