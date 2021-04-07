c6ddfd687955cf6624b6364ee703e244b6264860
yusseef <yussef.raouf11@gmail.com>
2021-04-07 21:06:14 +0200

    add home components style and links

--- blog-frontend/src/components/Home.js
+++ blog-frontend/src/components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const home = () =>(
-    <div>
-        home
+    <div className='container'>
+        <div className="jumbotron">
+            <h1 className="display-4">Blog app</h1>
+            <p className="lead">This is a simple blog app made by Django rest framweok & react.</p>
+            <hr className="my-4"/>
+            <p>Click the button below to check Blogs.</p>
+            <Link className="btn btn-primary btn-lg" to='/blog' role="button">Learn more</Link>
+        </div>
     </div>
 );
 
--- blog-frontend/src/components/Navbar.js
+++ blog-frontend/src/components/Navbar.js
@@ -2,9 +2,29 @@ import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 
 const navbar = () =>(
-    <nav>
-        Navbar
-    </nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" to='/'>Blog App</a>
+    <button
+     className="navbar-toggler" 
+     type="button" 
+     data-toggle="collapse" 
+     data-target="#navbarNav" 
+     aria-controls="navbarNav" 
+     aria-expanded="false" 
+     aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+            <li className="nav-item active">
+            <NavLink a className="nav-link" exact to ='/'>Home <span className="sr-only">(current)</span></NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink a className="nav-link" exact to ='/blog'>blog</NavLink>
+            </li>
+        </ul>
+    </div>
+  </nav>
 );
 
 export default navbar;
