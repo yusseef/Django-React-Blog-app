43123a4b5ffef93f6520ea11e6ff65a3bd016618
yusseef <yussef.raouf11@gmail.com>
2021-04-11 09:22:38 +0200

    add blogs page

--- blog-frontend/src/components/Blog.js
+++ blog-frontend/src/components/Blog.js
@@ -1,21 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import {Link} from 'react-router-dom'
-const Blog = () =>{
+  
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Blog = () => {
     const [blogs, setBlogs] = useState([]);
     const [featuredBlog, setFeaturedBlog] = useState([]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`);
                 setFeaturedBlog(res.data[0]);
                 console.log(res.data)
-                     
             }
-            catch(err) {
+            catch (err) {
 
             }
         }
+
         fetchData();
     }, []);
 
@@ -23,45 +26,79 @@ const Blog = () =>{
         const fetchBlogs = async () => {
             try {
                 const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/`);
-                setBlogs(res.data[0]);
-                     
+                setBlogs(res.data);
             }
-            catch(err) {
+            catch (err) {
 
             }
         }
+
         fetchBlogs();
     }, []);
 
-    const capitalizeFirstLetter = (word) =>{
+    const capitalizeFirstLetter = (word) => {
         if (word)
             return word.charAt(0).toUpperCase() + word.slice(1);
         return '';
     };
 
     const getBlogs = () => {
+        let list = [];
+        let result = [];
+        
+        blogs.map(blogPost => {
+            return list.push(
+                <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div className="col p-4 d-flex flex-column position-static">
+                        <strong className="d-inline-block mb-2 text-primary">{capitalizeFirstLetter(blogPost.category)}</strong>
+                        <h3 className="mb-0">{blogPost.title}</h3>
+                        <div className="mb-1 text-muted">{blogPost.month} {blogPost.day}</div>
+                        <p className="card-text mb-auto">{blogPost.excerpt}</p>
+                        <Link to={`/blog/${blogPost.slug}`} className="stretched-link">Continue reading</Link>
+                    </div>
+                    <div className="col-auto d-none d-lg-block">
+                        <img width='200' height='250' src={blogPost.thumbnail} alt='thumbnail' />
+                    </div>
+                </div>
+            );
+        });
 
+        for (let i = 0; i < list.length; i += 2) {
+            result.push(
+                <div key={i} className='row mb-2'>
+                    <div className='col-md-6'>
+                        {list[i]}
+                    </div>
+                    <div className='col-md-6'>
+                        {list[i+1] ? list[i+1] : null}
+                    </div>
+                </div>
+            )
+        }
+
+        return result;
     };
 
-    return(
+    return (
         <div className='container mt-3'>
             <div className="nav-scroller py-1 mb-2">
                 <nav className="nav d-flex justify-content-between">
-                <Link className="p-2 link-secondary" to="/category/world">World</Link>
-                <Link className="p-2 link-secondary" to="/category/environment">Environment</Link>
-                <Link className="p-2 link-secondary" to="/category/technology">Technology</Link>
-                <Link className="p-2 link-secondary" to="/category/design">Design</Link>
-                <Link className="p-2 link-secondary" to="/category/culture">Culture</Link>
-                <Link className="p-2 link-secondary" to="/category/business">Business</Link>
-                <Link className="p-2 link-secondary" to="/category/politics">Politics</Link>
-                <Link className="p-2 link-secondary" to="/category/opinion">Opinion</Link>
-                <Link className="p-2 link-secondary" to="/category/science">Science</Link>
-                <Link className="p-2 link-secondary" to="/category/health">Health</Link>
-                <Link className="p-2 link-secondary" to="/category/style">Style</Link>
-                <Link className="p-2 link-secondary" to="/category/travel">Travel</Link>
+                    <Link className="p-2 text-muted" to='/category/world'>World</Link>
+                    <Link className="p-2 text-muted" to='/category/environment'>Environment</Link>
+                    <Link className="p-2 text-muted" to='/category/technology'>Technology</Link>
+                    <Link className="p-2 text-muted" to='/category/design'>Design</Link>
+                    <Link className="p-2 text-muted" to='/category/culture'>Culture</Link>
+                    <Link className="p-2 text-muted" to='/category/business'>Business</Link>
+                    <Link className="p-2 text-muted" to='/category/politics'>Politics</Link>
+                    <Link className="p-2 text-muted" to='/category/opinion'>Opinion</Link>
+                    <Link className="p-2 text-muted" to='/category/science'>Science</Link>
+                    <Link className="p-2 text-muted" to='/category/health'>Health</Link>
+                    <Link className="p-2 text-muted" to='/category/style'>Style</Link>
+                    <Link className="p-2 text-muted" to='/category/travel'>Travel</Link>
                 </nav>
-        </div>
-        <div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+            </div>
+
+            <div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
                 <div className="col-md-6 px-0">
                     <h1 className="display-4 font-italic">{featuredBlog.title}</h1>
                     <p className="lead my-3">{featuredBlog.excerpt}</p>
@@ -72,13 +109,10 @@ const Blog = () =>{
                     </p>
                 </div>
             </div>
-        
-    </div>
-        
 
+            {getBlogs()}
+        </div>
     );
 };
-
-
 
 export default Blog;
